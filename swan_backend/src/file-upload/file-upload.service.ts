1761b58e30cc4a4547230a8da1fd1f1351d9db12
Paras Mogla <parasmogla0164@gmail.com>
2024-05-27 23:15:35 +0530

    changes

--- swan_backend/src/file-upload/file-upload.service.ts
+++ swan_backend/src/file-upload/file-upload.service.ts
@@ -6,25 +6,55 @@ import Swan from '@swan-admin/swan-ai-measurements';
 export class FileUploadService {
   private swan: Swan;
   constructor() {
-    this.swan = new Swan(process.env.API_KEY);
+    this.swan = new Swan(process.env.API_KEY, true);
   }
   async uploadFile(file: Express.Multer.File, fileUploadDto: FileUploadDto) {
     if (!file) {
       throw new BadRequestException('File not found!');
     }
+
+    const keyValuePairs = JSON.parse(fileUploadDto.arrayMetaData).reduce(
+      (acc, obj) => {
+        const key = Object.keys(obj)[0];
+        acc[key] = obj[key];
+        return acc;
+      },
+      {},
+    );
+    let customerConfig = null;
+    try {
+      customerConfig = await this.swan.custom.getCustomCustomerConfig(
+        keyValuePairs.customer_store_url,
+      );
+    } catch (e) {
+    }
+    if (!customerConfig) {
+      try {
+        let cusName = await this.swan.custom.createCustomer({
+          name: keyValuePairs.customer_store_url,
+          storeUrl: keyValuePairs.customer_store_url,
+          location: 'IE',
+          email: keyValuePairs.email,
+          emailsTier_1: null,
+          emailsTier_2: null,
+        });
+      } catch (e) {
+        throw new BadRequestException(e.response.data.message);
+      }
+    }
+
     try {
       const blob = new Blob([file.buffer], { type: file.mimetype });
       const files = new File([blob], file.originalname, {
         lastModified: Date.now(),
         type: file.mimetype,
       });
-
       const data = {
         scanId: fileUploadDto.scanId,
         file: files,
         arrayMetaData: JSON.parse(fileUploadDto.arrayMetaData),
       };
-      return await this.swan.fileUpload.uploadFileBackend(data);
+      return await this.swan.fileUpload.uploadFile(data);
     } catch (error) {
       throw new BadRequestException(error.message);
     }
